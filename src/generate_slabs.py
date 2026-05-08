38ba38cce00fa863fdd1aaf6319ef5df5b101b46
ohtake1823-source <ohtake1823@gmail.com>
2026-05-08 18:25:12 +0900

    Add initial slab generation script

---
+++ src/generate_slabs.py
@@ -0,0 +1,5 @@
+from pymatgen.core import Structure
+from pymatgen.core.surface import SlabGenerator
+from pymatgen.io.ase import AseAtomsAdaptor
+
+print("Ru-M slab generation workflow initialized")
